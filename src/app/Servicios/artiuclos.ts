e3d987a60e28a82f269ecc1a15135a4ca242357e
Rares <rngheru@gmail.com>
2025-10-30 11:41:42 +0100

    Servicio

---
+++ src/app/Servicios/artiuclos.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { ARTICULOS, Articulo } from "../Modelos/articulo";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ArticulosService {
+    constructor() {}
+
+    articulos:Articulo[]= ARTICULOS
+
+    getArticulos():Articulo[]{
+        return this.articulos
+    }
+
+    getArticulo(id:string){
+        let pos=this.articulos.findIndex(art=> art.id===id)
+        return this.articulos[pos]
+    }
+
+    postArticulo(articulo:Articulo){
+        let pos=this.articulos.findIndex(art=> art.id===articulo.id)
+        if(pos===-1){
+            this.articulos.push(articulo)
+        }
+        else
+            alert("El articulo ya existe")
+    }
+
+    putArticulo(articulo:Articulo){
+        let pos=this.articulos.findIndex(art=> art.id===articulo.id)
+        this.articulos[pos]=articulo
+    }
+
+    deleteArticulo(id:string){
+        let pos=this.articulos.findIndex(art=> art.id===id)
+        this.articulos.splice(pos,1)
+    }
+}
