cc2354652ccd562c1b9ac0afdfb9571b32a4fc07
Rares <rngheru@gmail.com>
2025-11-03 10:17:56 +0100

    Componente articulo

--- src/app/Components/modificar/modificar.ts
+++ src/app/Components/modificar/modificar.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+// src/app/Components/modificar/modificar.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Articulos } from '../../Servicios/articulosService';
+import { Articulo } from '../../Modelos/articulo';
+import { FormsModule } from '@angular/forms'; // (Teoría: Tema 3)
 
 @Component({
   selector: 'app-modificar',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule], // 1. Importar FormsModule
   templateUrl: './modificar.html',
-  styleUrl: './modificar.css',
+  styleUrl: './modificar.css'
 })
-export class Modificar {
+export class Modificar implements OnInit {
+  
+  public articulo: Articulo = {
+    id: '',
+    nombre: '',
 
+    precio: 0,
+    unidades: 0 
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private articulos: Articulos
+  ) { }
+
+  ngOnInit(): void {
+  
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        
+        const articuloEncontrado = this.articulos.getArticulo(id);
+        if (articuloEncontrado) {
+          
+          this.articulo = { ...articuloEncontrado };
+        } else {
+          
+          this.router.navigate(['/articulos']);
+        }
+      }
+    });
+  }
+
+  
+  modificar(): void {
+    this.articulos.putArticulo(this.articulo);
+    this.router.navigate(['/articulos']);
+  }
+
+
+  cancelar(): void {
+    this.router.navigate(['/articulos']);
+  }
 }
--- src/app/Modelos/articulo.ts
+++ src/app/Modelos/articulo.ts
@@ -1,7 +1,6 @@
 export interface Articulo {
     id:string;
     nombre:string;
-    descripcion:string;
     precio:number;
     unidades:number
 }
@@ -10,38 +9,20 @@ export const ARTICULOS: Array<Articulo> = [
     {
         id: 'm1',
         nombre: 'Articulo 1',
-        descripcion: 'Descripcion del Articulo 1',
         precio: 100,
         unidades: 10
     },
     {
         id: 'm2',
-        nombre: 'Articulo 2',
-        descripcion: 'Descripcion del Articulo 2',
+        nombre: 'Articulo 2',  
         precio: 200,
         unidades: 20
     },
     {
         id: 'm3',
         nombre: 'Articulo 3',
-        descripcion: 'Descripcion del Articulo 3',
         precio: 300,
         unidades: 30
     },
 
-    {
-        id: 'm4',
-        nombre: 'Articulo 3',
-        descripcion: 'Descripcion del Articulo 3',
-        precio: 300,
-        unidades: 30
-    },
-
-    {
-        id: 'm5',
-        nombre: 'Articulo 3',
-        descripcion: 'Descripcion del Articulo 3',
-        precio: 300,
-        unidades: 30
-    }
 ];
