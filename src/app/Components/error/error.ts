8a7b26a04285a325c92d135d45b1acc77d685eb5
Rares <rngheru@gmail.com>
2025-11-03 10:38:40 +0100

    Practica acabada

--- src/app/Components/error/error.ts
+++ src/app/Components/error/error.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-error',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink], 
   templateUrl: './error.html',
-  styleUrl: './error.css',
+  styleUrl: './error.css'
 })
 export class Error {
 
--- src/app/Components/nuevo/nuevo.ts
+++ src/app/Components/nuevo/nuevo.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Articulos } from '../../Servicios/articulosService'; 
+import { Articulo } from '../../Modelos/articulo';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './nuevo.html',
-  styleUrl: './nuevo.css',
+  styleUrl: './nuevo.css'
 })
 export class Nuevo {
+  
+  public nuevoArticulo: Articulo = {
+    id: '',
+    nombre: '',
+    precio: 0,
+    unidades: 0
+  };
 
+  constructor(
+    private router: Router,
+    private articulosService: Articulos
+  ) { }
+
+  grabar(): void {
+
+    this.articulosService.postArticulo(this.nuevoArticulo);
+    this.router.navigate(['/articulos']);
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/articulos']);
+  }
 }
