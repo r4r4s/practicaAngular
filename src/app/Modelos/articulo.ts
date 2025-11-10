778015e2ec655c33bb0f3d870e4cbef089832b29
Rares <rngheru@gmail.com>
2025-11-10 13:38:00 +0100

    Creación json

--- src/app/Modelos/articulo.ts
+++ src/app/Modelos/articulo.ts
@@ -6,23 +6,6 @@ export interface Articulo {
 }
 
 export const ARTICULOS: Array<Articulo> = [
-    {
-        id: 'm1',
-        nombre: 'Articulo 1',
-        precio: 100,
-        unidades: 10
-    },
-    {
-        id: 'm2',
-        nombre: 'Articulo 2',  
-        precio: 200,
-        unidades: 20
-    },
-    {
-        id: 'm3',
-        nombre: 'Articulo 3',
-        precio: 300,
-        unidades: 30
-    },
+   
 
 ];
