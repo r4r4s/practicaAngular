f747b6a9aee9e337964c4314a0ffdd101842176d
Rares <rngheru@gmail.com>
2025-11-10 13:11:27 +0100

    Login

--- src/app/Components/login/login.ts
+++ src/app/Components/login/login.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { LoginService } from '../../Servicios/login/loginService'; 
 
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -15,12 +16,13 @@ export class Login {
 
   public usuario: string = '';
   public contrasena: string = '';
+
   
   public errorLogin: boolean = false;
 
   constructor(
     private router: Router,
-    private loginService: LoginService // 
+    private loginService: LoginService 
   ) { }
 
   login(): void {
@@ -29,6 +31,7 @@ export class Login {
     const exito = this.loginService.login(this.usuario, this.contrasena);
 
     if (exito) {
+
      
       this.errorLogin = false;
       this.router.navigate(['/inicio']);
