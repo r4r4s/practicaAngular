9d7ecb2343a7362794c9d9849f876d7d833563f4
Rares <rngheru@gmail.com>
2025-11-11 11:35:57 +0100

    Fix article detail view data loading

--- src/app/Components/ver/ver.ts
+++ src/app/Components/ver/ver.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Articulos } from '../../Servicios/articulos/articulosService';
 import { Articulo } from '../../Modelos/articulo';
 import { CurrencyPipe } from '@angular/common';
 import { CommonModule } from '@angular/common';
-import { Subscription } from 'rxjs';
+import { Observable, catchError, of, shareReplay, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-ver',
@@ -12,40 +12,45 @@ import { Subscription } from 'rxjs';
   imports: [CommonModule, CurrencyPipe],
   providers: [Articulos],
   templateUrl: './ver.html',
-  styleUrl: './ver.css'
+  styleUrls: ['./ver.css']
 })
-export class VerComponent implements OnInit, OnDestroy {
-  articulo?: Articulo;
-  private subscriptions = new Subscription();
+export class VerComponent {
+  articulo$: Observable<Articulo | null>;
 
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private articulosService = inject(Articulos);
 
-  ngOnInit(): void {
-    const paramsSub = this.route.params.subscribe((params: any) => {
-      const id = params['id'];
-      if (id) {
-        const getArticuloSub = this.articulosService.getArticulo(id).subscribe({
-          next: (data: Articulo) => {
-            this.articulo = data;
-          },
-          error: (err: any) => {
+  constructor() {
+    this.articulo$ = this.route.params.pipe(
+      switchMap((params) => {
+        const id = params['id'];
+        if (!id) {
+          return of(null);
+        }
+
+        return this.articulosService.getArticulo(id).pipe(
+          tap((data: Articulo) => {
+            if (!data) {
+              console.warn(`Artículo con id ${id} no encontrado.`);
+            }
+          }),
+          catchError((err) => {
             console.error('Error al cargar artículo:', err);
             this.router.navigate(['/error']);
-          }
-        });
-        this.subscriptions.add(getArticuloSub);
-      }
-    });
-    this.subscriptions.add(paramsSub);
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
+            return of(null);
+          })
+        );
+      }),
+      shareReplay(1)
+    );
   }
 
   volver(): void {
     this.router.navigate(['/articulos']);
+  }
+
+  useDefaultImage(event: Event): void {
+    (event.target as HTMLImageElement).src = 'assets/default.jpg';
   }
 }
